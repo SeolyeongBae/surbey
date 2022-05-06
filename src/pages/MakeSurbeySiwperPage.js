0ca4d1d765b080e3f7cf9da781b6c57a7fcda4f6
pell13 <peixueying@gmail.com>
2022-05-06 16:43:16 +0900

    Add Style on Swiper

--- src/pages/MakeSurbeySiwperPage.js
+++ src/pages/MakeSurbeySiwperPage.js
@@ -7,6 +7,20 @@ import "swiper/css/pagination";
 
 SwiperCore.use([Navigation, Pagination]);
 
+const testStyle = {
+  height: "10em",
+  fontSize: "1rem",
+  lineHeight: 1.5,
+  display: "flex",
+};
+
+const innerStyle = {
+  width: "10em",
+  border: "1px solid teal",
+  borderRadius: ".25rem",
+  margin: "0 auto",
+};
+
 function MakeSurbeySwiperPage({ index }) {
   return (
     <div>
@@ -18,7 +32,11 @@ function MakeSurbeySwiperPage({ index }) {
         navigation
         pagination={{ clickable: true }}
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>
+          <div style={testStyle}>
+            <div style={innerStyle}>가운데 정렬 좀 시켜주세요.</div>
+          </div>
+        </SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
