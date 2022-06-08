9c612d78a43f4474f39750159a60423125b7bb9a
pell13 <peixueying@gmail.com>
2022-06-09 00:55:07 +0900

    응답 완료 페이지

--- src/components/submitAnswerSurvey.js
+++ src/components/submitAnswerSurvey.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const innerStyle = {
   width: "18rem",
@@ -8,25 +8,42 @@ const innerStyle = {
 };
 
 function SubmitAnswerSurvey({}) {
+  const [isSubmited, setIsSubmited] = useState(false);
+
+  const onClick = () => {
+    setIsSubmited(() => true);
+  };
+
   return (
     <>
       <div
         style={innerStyle}
         className={"flex justify-center items-center flex-col bg-gray-50"}
       >
-        <div className="py-1 px-2 rounded-lg text-sky-600  text-2xl font-bold">
-          응답이 완료되었습니다.
-        </div>
-        <div> 제출하러 가볼까요?</div>
-
-        <div className="button-container my-5 flex flex-col">
-          <button
-            name={0}
-            className="py-5 px-5 my-2 font-semibold rounded-lg text-sky-600 bg-blue-100 hover:bg-blue-300 rounded-md  "
-          >
-            제출하기
-          </button>
-        </div>
+        {isSubmited ? (
+          <>
+            {" "}
+            <div className="py-1 px-2 rounded-lg text-sky-600  text-2xl font-bold">
+              응답이 완료되었습니다.
+            </div>
+            <div> 참여해주셔서 감사합니다.</div>
+          </>
+        ) : (
+          <>
+            <div className="py-1 px-2 rounded-lg text-sky-600  text-2xl font-bold">
+              응답이 완료되었습니다.
+            </div>
+            <div> 제출하러 가볼까요?</div>
+            <div className="button-container my-5 flex flex-col">
+              <button
+                onClick={onClick}
+                className="py-5 px-5 my-2 font-semibold rounded-lg text-sky-600 bg-blue-100 hover:bg-blue-300 rounded-md  "
+              >
+                제출하기
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
