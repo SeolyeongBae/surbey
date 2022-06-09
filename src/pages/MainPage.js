82f84af39de3520a2b0b0a62d169e3478b4ae673
pell13 <peixueying@gmail.com>
2022-06-09 09:25:21 +0900

    메인 페이지 생성

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -4,14 +4,23 @@ import { Link } from "react-router-dom";
 function MainPage() {
   return (
     <>
-      <div>
-        <Link to="/response/f6b3243e-e348-4673-a0ee-1fc38dcbbca8">
-          검색하기
+      <div className="flex flex flex-col px-10 py-10">
+        <div className="mt-40 mb-5 w-20 content-center ">
+          <img
+            src="https://www.seekpng.com/png/full/890-8908836_election-officials-need-timely-information-about-data-ballot.png"
+            alt="투표"
+          />
+        </div>
+        <h3 className="py-1 rounded-lg text-2xl font-bold">
+          쉽고 편하고 꼼꼼한 설문 만들기
+        </h3>
+
+        <div> 세상에서 가장 쉬운 설문 제작에 도전해요! </div>
+        <Link to="/edit/info">
+          <button className="mt-5 py-2 px-4 font-semibold rounded-lg text-white bg-blue-500 hover:bg-blue-700">
+            설문 만들기
+          </button>
         </Link>
-      </div>
-      <div> 설문을 만들고 싶으신가요~?</div>
-      <div>
-        <Link to="/edit/info"> 질문 만들기 </Link>
       </div>
     </>
   );
